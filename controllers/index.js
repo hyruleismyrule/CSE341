9c54d597625afe106507514ec185fbfef4f13cfc
hyruleismyrule <hyruleismyrule@gmail.com>
2022-09-15 17:37:24 -0400

    working! removed comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,82 +1,9 @@
-// // Return Text
-
-// const text = (req, res) => {
-//     res.send("Cynthia Rawlings");
-// }
-
-// module.exports = { text };
-
-// function sendText() {
-//     const index = (_req, res) => {
-//         res.send("Cynthia Rawlings");
-//     };
-// }
-
-// const index = (_req, res) => {
-//     res.send("Cynthia Rawlings");
-// };
-
-// module.exports = sendText;
-
-
-
-// function messageWindow() {
-//     const routes = require('express').Router();
-
-//     routes.get('/', (req, res) => {
-//         res.send('Cynthia Rawlings - Controllers');
-//     })
-// }
-
-// function messageWindow() {
-//     const routes = require('express').Router();
-
-//     routes.get('/', (req, res) => {
-//         res.send('Cynthia Rawlings - Controllers');
-//     })
-// }
-
-// function messageWindow() {
-//     const index = (req, res) => {
-//         res.send('Cynthia Rawlings - Controllers');
-//     }    
-// }
-
-// function log() {
-//     console.log("Controllers is logging");
-// }
-
-// module.exports.log = log;
-// module.exports.messageWindow = messageWindow;
-// const routes = require('express').Router();
-
-// const index = (_req, res) => {
-//     res.send('Cynthia Rawlings - Controllers');
-// } 
-
-// module.exports = { index }; 
-
-
-
-
 // ***************
 // Controllers
 // ***************
 
-
-// function messageWindow() {
-//     const index = (req, res) => {
-//         res.send('Cynthia Rawlings - Controllers');
-//     }    
-//     return index;
-// }
-
-// // module.exports.messageWindow = messageWindow();
-// module.exports = { messageWindow };
-
-const index = (_req, res) => {
-    res.send("Cynthia Rawlings");
+const index = (request, response) => {
+    response.send("Cynthia Rawlings");
 };
 
-module.exports = { index };
-
+module.exports = index;
--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use('/', require('./routes'))
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-
-// https://github.com/hyruleismyrule/CSE341.git
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
