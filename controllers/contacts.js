1b48206aa6f50178a0d67243b05443ddcc7dc65a
hyruleismyrule <hyruleismyrule@gmail.com>
2022-09-23 19:40:55 -0400

    All and single connection working

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -37,7 +37,7 @@ const mongodb = require('../connections/index');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db('cse341').collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -48,7 +48,7 @@ const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db()
+    .db('cse341')
     .collection('contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongodb = require('./connections/index');
 
 app.use('/', require('./routes'));
 // app.use('/', require('./routes/contacts'));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+  });
