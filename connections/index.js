1b9f93012c890fef817f83f267d80a4507a436c0
hyruleismyrule <hyruleismyrule@gmail.com>
2022-09-23 22:44:07 -0400

    changed it back, logs to console

--- connections/index.js
+++ connections/index.js
@@ -9,18 +9,14 @@ const MongoClient = require('mongodb').MongoClient;
 
 let _db;
 
-let codeUsername = process.env.DB_USERNAME;
-let codePass = process.env.DB_PASSWORD;
-let secureString = "mongodb+srv://" + codeUsername + ":" + codePass + "@cluster0.zxiazbs.mongodb.net/?retryWrites=true&w=majority";
-console.log(secureString);
+console.log(process.env.MONGODB_URI);
 
 const initDb = (callback) => {
   if (_db) {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
-  // MongoClient.connect(process.env.MONGODB_URI)
-  MongoClient.connect(secureString)
+  MongoClient.connect(process.env.MONGODB_URI)
     .then((client) => {
       _db = client;
       callback(null, _db);
