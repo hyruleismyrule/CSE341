e7263a65c7c1c349e0258aacd0e9451b530e343c
hyruleismyrule <hyruleismyrule@gmail.com>
2022-09-23 22:32:48 -0400

    changed db string again

--- connections/index.js
+++ connections/index.js
@@ -9,12 +9,18 @@ const MongoClient = require('mongodb').MongoClient;
 
 let _db;
 
+let codeUsername = process.env.DB_USERNAME;
+let codePass = process.env.DB_PASSWORD;
+let secureString = "mongodb+srv://" + codeUsername + ":" + codePass + "@cluster0.zxiazbs.mongodb.net/?retryWrites=true&w=majority";
+console.log(secureString);
+
 const initDb = (callback) => {
   if (_db) {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
-  MongoClient.connect(process.env.MONGODB_URI)
+  // MongoClient.connect(process.env.MONGODB_URI)
+  MongoClient.connect(secureString)
     .then((client) => {
       _db = client;
       callback(null, _db);
