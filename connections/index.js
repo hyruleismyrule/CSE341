95c96336232bf750aafac21710cc2702ada60de5
hyruleismyrule <hyruleismyrule@gmail.com>
2022-09-23 22:10:18 -0400

    reverted back to secure string

--- connections/index.js
+++ connections/index.js
@@ -14,7 +14,8 @@ const initDb = (callback) => {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
-  MongoClient.connect(process.env.MONGODB_URI)
+  // MongoClient.connect(process.env.MONGODB_URI)
+  MongoClient.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zxiazbs.mongodb.net/?retryWrites=true&w=majority`)
     .then((client) => {
       _db = client;
       callback(null, _db);
